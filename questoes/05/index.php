a5fbe9c35e4e027080954463ea4655082959362b
valsouza05 <valdinei.morais@estudante.ifto.edu.br>
2025-09-25 20:45:10 -0300

    Update index.php

--- questoes/05/index.php
+++ questoes/05/index.php
@@ -1,25 +1,28 @@
 <!DOCTYPE html>
-<html lang="pt-br">
-
+<html lang="pt-BR">
 <head>
-    <meta charset="UTF--8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página Inicial - Exercícios PHP</title>
-    <link rel="stylesheet" href="./../../styles.css">
+    <meta charset="UTF-8">
+    <title>Questão 5 - Maior Número</title>
 </head>
-
 <body>
+    <h2>Questão 5: Maior entre Três Números</h2>
 
-    <header>
-        <h2>Questão 05: Maior de Três Números</h2>
-    </header>
+    <form method="post">
+        <input type="number" name="n1" placeholder="Número 1" required><br><br>
+        <input type="number" name="n2" placeholder="Número 2" required><br><br>
+        <input type="number" name="n3" placeholder="Número 3" required><br><br>
+        <button type="submit">Verificar</button>
+    </form>
 
-    <main>
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        $n1 = $_POST["n1"];
+        $n2 = $_POST["n2"];
+        $n3 = $_POST["n3"];
 
-    <!-- implementação da solução -->
-     
-    </main>
+        $maior = max($n1, $n2, $n3);
+        echo "<p>O maior número é <strong>$maior</strong></p>";
+    }
+    ?>
 </body>
-
-
 </html>
